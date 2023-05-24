de7a5cb97b5cd96c15b619437db81e4e6b69cd63
Lucky Khounvongsa <luckykhounvongsa@gmail.com>
2023-05-24 04:05:17 -0400

    Amélioration de l'exercice 1

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("birthday").addEventListener("change", function() {
+    var birthday = new Date(this.value);
+    var today = new Date();
+    var age = today.getFullYear() - birthday.getFullYear();
+    var m = today.getMonth() - birthday.getMonth();
+  
+    if (m < 0 || (m === 0 && today.getDate() < birthday.getDate())) {
+        age--;
+    }
+  
+    document.getElementById("age").textContent = "Âge : " + age;
+});
