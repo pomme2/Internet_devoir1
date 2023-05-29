6eb14040e24c718124e101f5e7888816de1a35c7
Lucky Khounvongsa <luckykhounvongsa@gmail.com>
2023-05-28 22:32:18 -0400

    Exercice 2 (Bug avec les commentaires et placement des boutons)
    Longue commentaire deplace les boutons de la ligne et les commentaires pour Utilisateurs 1 et 2 ne fonctionnent pas...

--- partie2/script.js
+++ partie2/script.js
@@ -1,90 +1,54 @@
-function addToQueue() {
-  let nameInput = document.getElementById("nameInput");
-  let queue = document.getElementById("queue");
-  if(nameInput.value.trim() !== "") {
-      let userItem = document.createElement("li");
-      userItem.classList.add("list-group-item", "d-flex", "justify-content-between");
+$(document).ready(function () {
+  let count = 0; // Utilisé pour créer un identifiant unique pour chaque bouton "Commenter"
 
-      let userText = document.createElement("span");
-      userText.textContent = nameInput.value;
-
-      let buttonGroup = document.createElement("div");
-
-      let commentButton = document.createElement("button");
-      commentButton.classList.add("btn", "btn-info", "comment-btn", "me-2");
-      commentButton.textContent = "Commenter";
-      commentButton.setAttribute("data-bs-toggle", "modal");
-      commentButton.setAttribute("data-bs-target", "#commentModal");
-      commentButton.addEventListener("click", function() {
-          window.currentUserItem = userItem;
-      });
-
-      let deleteButton = document.createElement("button");
-      deleteButton.classList.add("btn", "btn-danger", "delete-btn");
-      deleteButton.textContent = "X";
-      deleteButton.setAttribute("data-bs-toggle", "modal");
-      deleteButton.setAttribute("data-bs-target", "#deleteModal");
-      deleteButton.addEventListener("click", function() {
-          window.currentUserItem = userItem;
-      });
-
-      buttonGroup.appendChild(commentButton);
-      buttonGroup.appendChild(deleteButton);
-
-      userItem.appendChild(userText);
-      userItem.appendChild(buttonGroup);
-
-      queue.appendChild(userItem);
-
-      nameInput.value = "";
-
-      let addUserModal = bootstrap.Modal.getInstance(document.getElementById("addModal"));
-      addUserModal.hide();
-  }
-}
-
-
-function updateComment() {
-  let commentInput = document.getElementById("commentInput");
-  let comment = commentInput.value.trim();
-
-  if (comment !== "") {
-      if (window.currentUserItem) {
-          let existingComment = window.currentUserItem.querySelector(".user-comment");
-
-          // Si un commentaire existe, modifier
-          if (existingComment) {
-              existingComment.textContent = `"${comment}"`;
-          } else {
-              let commentSpan = document.createElement("span");
-              commentSpan.classList.add("user-comment", "ms-2");
-              commentSpan.textContent = `"${comment}"`;
-              window.currentUserItem.insertBefore(commentSpan, window.currentUserItem.children[1]);
-          }
+  // Ajouter un utilisateur à la file d'attente
+  $("#addModal .btn-primary").click(function () {
+      var userName = $("#nameInput").val();
+      if (userName === '') {
+          alert('Veuillez entrer votre nom.');
+          return;
       }
-      commentInput.value = "";
-      let commentModal = bootstrap.Modal.getInstance(document.getElementById("commentModal"));
-      commentModal.hide();
-  }
-}
 
-function selectUserForComment(userItem) {
-  window.currentUserItem = userItem;
-  let commentModal = bootstrap.Modal.getInstance(document.getElementById("commentModal"));
-  commentModal.show();
-}
+      // Incrémenter le compteur et utiliser sa valeur pour l'identifiant du bouton "Commenter"
+      count++;
 
-let commentButtons = document.getElementsByClassName("comment-btn");
-for (let i = 0; i < commentButtons.length; i++) {
-  commentButtons[i].addEventListener("click", function() {
-      let userItem = this.closest(".list-group-item");
-      selectUserForComment(userItem);
+      var newUser = $("<li class='list-group-item d-flex justify-content-between align-items-center'>"
+                      + userName
+                      + "<span id='comment-" + count + "' class='comment-text mx-3'></span>"
+                      + "<div>"
+                          + "<button type='button' class='btn btn-info comment-btn me-2' data-bs-toggle='modal' data-bs-target='#commentModal' data-id='" + count + "'>Commenter</button>"
+                          + "<button type='button' class='btn btn-danger delete-btn'>X</button>"
+                      + "</div>"
+                  + "</li>");
+
+      $("#queue").append(newUser);
+      $("#addModal").modal('hide');
+      $("#nameInput").val('');
   });
-}
 
-function deleteItem() {
-  window.currentUserItem.remove();
-  let deleteUserModal = bootstrap.Modal.getInstance(document.getElementById("deleteModal"));
-  deleteUserModal.hide();
-}
-document.getElementById("confirmDeleteButton").addEventListener("click", deleteItem);
+  // Supprimer un utilisateur de la file d'attente
+  $(document).on('click', '.delete-btn', function () {
+      $(this).closest('li').remove();
+  });
+
+  // Ouvrir le modal de commentaire et pré-remplir le champ de commentaire avec le commentaire existant
+  $(document).on('click', '.comment-btn', function () {
+      let id = $(this).data('id');
+      let existingComment = $("#comment-" + id).text();
+      $("#commentInput").val(existingComment.replace(/"/g, ''));
+      $("#commentModal .btn-primary").data('id', id); // Stocker l'ID sur le bouton "Ajouter/Modifier" pour l'utiliser plus tard
+  });
+
+  // Ajouter ou modifier un commentaire
+  $("#commentModal .btn-primary").click(function () {
+      let id = $(this).data('id');
+      let comment = $("#commentInput").val();
+
+      // Ajouter le commentaire à côté du nom de l'utilisateur
+      $("#comment-" + id).text('"' + comment + '"');
+
+      // Fermer le modal et réinitialiser le champ de commentaire
+      $("#commentModal").modal('hide');
+      $("#commentInput").val('');
+  });
+});
